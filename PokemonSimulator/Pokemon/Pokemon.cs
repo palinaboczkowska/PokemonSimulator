5441a5351b70611c0b193b3b3cf53d1daf5d5711
Palina Boczkowska <palina.boczkowska@gmail.com>
2025-11-12 09:01:52 +0100

    Lade till validering för namn och level i Pokemon klassen

--- PokemonSimulator/Pokemon/Pokemon.cs
+++ PokemonSimulator/Pokemon/Pokemon.cs
@@ -6,9 +6,28 @@ namespace PokemonSimulator.Pokemon
         private string _name;
         private int _level;
 
-        public string Name { get; set; } //lägga till validering för 2-15 tecken
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (value.Length < 2 || value.Length > 15)
+                    throw new ArgumentException("Namnet måste vara mellan 2 och 15 tecken långt.");
+                _name = value;
+            }
+        }
         
-        public int Level { get; set; }//lägga till validering för >=1
+        public int Level
+        {
+            get => _level;
+            set
+            {
+                if (value < 1)
+                    throw new ArgumentException("Level måste vara minst 1.");
+                _level = value;
+
+            }
+        }
         public ElementType ElementType { get; set; } //set protected?
         public List<Attack> Attacks { get; set; } //set inte behövs?
 
