11005dffc5eb1ac46ac84fdba7268a117168a948
Palina Boczkowska <palina.boczkowska@gmail.com>
2025-11-12 08:46:27 +0100

    Skapade flera instanser i Program.cs

--- PokemonSimulator/Program.cs
+++ PokemonSimulator/Program.cs
@@ -2,13 +2,24 @@
 using PokemonSimulator;
 using PokemonSimulator.Pokemon;
 
-var pokemons = new List<Pokemon>
+Attack flamethrower = new Attack("Flamethrower", ElementType.Fire, 12);
+Attack ember = new Attack("Ember", ElementType.Fire, 6);
+Attack waterGun = new Attack("Water Gun", ElementType.Water, 5);
+Attack leafBlade = new Attack("Leaf Blade", ElementType.Grass, 4);
+
+Pokemon charmander = new Charmander(1, new List<Attack> { flamethrower, ember});
+Squirtle squirtle = new Squirtle(2, new List<Attack>() { waterGun });
+Bulbasaur bulbasaur = new Bulbasaur(3, new List<Attack> { leafBlade });
+
+List<Pokemon> pokemons = new List<Pokemon> //eller med hjälp av .Add()
 {
-
-    //skapa pokemons
-
+charmander,
+squirtle,
+bulbasaur
 };
 
-Attack flamethrower = new Attack("Flamethrower", ElementType.Fire, 12);
 
-Attack ember = new Attack("Ember", ElementType.Fire, 6);
+
+
+
+
